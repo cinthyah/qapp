ced78e93174d2617ec47a65a05da08e571b9999c
Demo User <demouser@MacBook-Pro.local>
2018-07-30 15:24:43 -0700

    Added more basic stuff to main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import webapp2
 import json
 from google.appengine.api import users
+from twilio.rest import Client
+
+account_sid = "ACfe3c09107ca923f7c8425fc58cbf0fc4"
+auth_token = "e32ae179c853e667d8f5fc135d89c0aa"
+client = Client(account_sid, auth_token)
 
 
 
