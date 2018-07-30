154c42f61fda3005b6a1031eb47bf30f8dba7d48
Demo User <demouser@MacBook-Pro.local>
2018-07-30 15:33:37 -0700

    adding twilio to our main,py

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ account_sid = "ACfe3c09107ca923f7c8425fc58cbf0fc4"
 auth_token = "e32ae179c853e667d8f5fc135d89c0aa"
 client = Client(account_sid, auth_token)
 
+message = client.messages.create(
+                              body='Your table will be ready in five minutes',
+                              from_='17472325261',
+                              to='13107176463'#have to add variable here 
+                          )
+
+print(message.sid)
 
 
 class MainPage(webapp2.RequestHandler):
