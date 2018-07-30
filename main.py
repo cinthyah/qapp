50bc5dc3ba7d74176d666f8e52e60e26f6a429e7
Demo User <demouser@MacBook-Pro.local>
2018-07-30 15:22:06 -0700

    Added basic stuff to main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import webapp2
+import json
 from google.appengine.api import users
+
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
