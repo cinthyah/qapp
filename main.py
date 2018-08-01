ac98b4197b0dba56aaf476bf48c29a5cddc9afcc
Mannie Chomsin <manniec00@gmail.com>
2018-07-31 17:31:21 -0700

    update main.py if else for users

--- main.py
+++ main.py
@@ -36,12 +36,19 @@ class LoginHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            url = users.create_logout_url('/')
-            
+            log_url = users.create_logout_url('/')
+            if user
+            self.redirect('/r_home')
+
         else:
-            url = users.create_login_url('/')
+            log_url = users.create_login_url('/')
         new_r_template=jinja_current_directory.get_template("templates/login2.html")
-        self.response.write(new_r_template.render({'url':url}))
+        self.response.write(new_r_template.render({'log_url': log_url}))
+
+class HomeHandler(webapp2.RequestHandler):
+    def get(self):
+
+
 
 class RestNewHandler(webapp2.RequestHandler):
     def get(self):
@@ -49,16 +56,20 @@ class RestNewHandler(webapp2.RequestHandler):
         self.response.write(new_r_template.render())
 
     def post(self):
+        user = users.get_current_user()
         Restaurant(name = self.request.get('name_r'),
             phone = self.request.get('phone_r'),
-            street_address= self.request.get('street'),
+            street_address = self.request.get('street'),
             city = self.request.get('city'),
             state = self.request.get('state'),
             zip_code = self.request.get('zip'),
+            user = user.user_email(),
         ).put()
+
 
 
 app = webapp2.WSGIApplication([
     ('/',LoginHandler),
+    ('/r_home', HomeHandler),
     ('/new_rest', RestNewHandler),
 ], debug=True)
