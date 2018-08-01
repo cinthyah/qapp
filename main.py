40a242bb65b76f9a93801355fa27f374a8336dfc
Mannie Chomsin <manniec00@gmail.com>
2018-08-01 16:26:53 -0700

    fix attribute error

--- main.py
+++ main.py
@@ -138,19 +138,19 @@ class LoadDataHandler(webapp2.RequestHandler):
 
 class ActiveQHandler(webapp2.RequestHandler):
     def get (self):
-        user = self.request.get_current_user()
+        user = users.get_current_user()
         restaurant = Restaurant.query(Restaurant.user == user.email()).fetch()
         if restaurant:
-             restaurant = restaurant[0]
-             waits = Wait.query(Wait.restaurant_key == restaurant.key).order().fetch()
-             template_vars = {
-                "waits" : waits,
-                "restaurant":restaurant,
+            restaurant = restaurant[0]
+            waits = Wait.query(Wait.restaurant_key == restaurant.key).order().fetch()
+            template_vars = {
+            "waits" : waits,
+            "restaurant":restaurant,
              }
-             activeq_template = jinja_current_directory.get_template("templates/active_q.html")
-             self.response.write(activeq_template.render(template_vars))
-         else:
-             self.response.write(users.create_logout_url('/'))
+            activeq_template = jinja_current_directory.get_template("templates/active_q.html")
+            self.response.write(activeq_template.render(template_vars))
+        else:
+            self.response.write(users.create_logout_url('/'))
 
 class DeleteWaitHandler(webapp2.RequestHandler):
     def get(self):
