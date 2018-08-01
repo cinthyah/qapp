cb72ef1c0dc918c2fe590a02a13d5d72200e26a8
Mannie Chomsin <manniec00@gmail.com>
2018-08-01 15:35:56 -0700

    edit seed_data to include users, and fix active q handler

--- seed_q.py
+++ seed_q.py
@@ -2,7 +2,7 @@ from models import Restaurant, Table, Wait
 
 def seed_data():
     #restaurants
-    Shake_key = Restaurant(name = "Shake Shack", phone = "3234883010", street_address = "8520 Santa Monica Blvd", city = "West Hollywood", state = "CA", zip_code = "90069", user = "example@gmail.com").put()
+    shake_key = Restaurant(name = "Shake Shack", phone = "3234883010", street_address = "8520 Santa Monica Blvd", city = "West Hollywood", state = "CA", zip_code = "90069", user = "example@gmail.com").put()
     in_key = Restaurant(name = "In n Out", phone = "8007861000", street_address = "13425 Washington Blvd", city = "Marina Del Rey", state = "CA", zip_code = "90292", user = "example@gmail.com").put()
     cheese_key = Restaurant(name = "Cheescake Factory", phone = "3102601296", street_address = "395 Santa Monica Place", city = "Santa Monica", state = "CA", zip_code = "90401", user = "example@gmail.com").put()
     blaze_key = Restaurant(name = "Blaze Pizza", phone = "3103400638", street_address = "4114 Sepulveda Blvd", city = "Culver City", state = "CA", zip_code = "90230", user = "example@gmail.com").put()
@@ -22,14 +22,14 @@ def seed_data():
     cheese3_table_key = Table(max = "2", min = "1", full = False).put()
 
     #wait
-    shake_c1_key = Wait(customer = "John Doe", phone = "3107176463", party_size = "5").put()
-    shake_c2_key = Wait(customer = "Jane Doe", phone = "3107176463", party_size = "2").put()
-    shake_c3_key = Wait(customer = "Bob", phone = "3107176463", party_size = "4").put()
+    shake_c1_key = Wait(customer = "John Doe", phone = "3107176463", party_size = "5", restaurant_key=shake_key).put()
+    shake_c2_key = Wait(customer = "Jane Doe", phone = "3107176463", party_size = "2", restaurant_key=shake_key).put()
+    shake_c3_key = Wait(customer = "Bob", phone = "3107176463", party_size = "4", restaurant_key=shake_key).put()
 
-    in_c1_key = Wait(customer = "John Doe", phone = "3107176463", party_size = "5").put()
-    in_c2_key = Wait(customer = "Jane Doe", phone = "3107176463", party_size = "2").put()
-    in_c3_key = Wait(customer = "Bob", phone = "3107176463", party_size = "4").put()
+    in_c1_key = Wait(customer = "John Doe", phone = "3107176463", party_size = "5", restaurant_key=in_key).put()
+    in_c2_key = Wait(customer = "Jane Doe", phone = "3107176463", party_size = "2", restaurant_key=in_key).put()
+    in_c3_key = Wait(customer = "Bob", phone = "3107176463", party_size = "4", restaurant_key=in_key).put()
 
-    cheese_c1_key = Wait(customer = "John Doe", phone = "3107176463", party_size = "5").put()
-    cheese_c2_key = Wait(customer = "Jane Doe", phone = "3107176463", party_size = "2").put()
-    cheese_c3_key = Wait(customer = "Bob", phone = "3107176463", party_size = "4").put()
+    cheese_c1_key = Wait(customer = "John Doe", phone = "3107176463", party_size = "5", restaurant_key=cheese_key).put()
+    cheese_c2_key = Wait(customer = "Jane Doe", phone = "3107176463", party_size = "2", restaurant_key=cheese_key).put()
+    cheese_c3_key = Wait(customer = "Bob", phone = "3107176463", party_size = "4", restaurant_key=cheese_key).put()
