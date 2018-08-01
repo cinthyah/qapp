468155d741b3f3e05f754336d2ba601aedb8b836
Mason Hendrick <super40111@gmail.com>
2018-08-01 11:33:41 -0700

    Reverted a name change

--- seed_q.py
+++ seed_q.py
@@ -2,10 +2,10 @@ from models import Restaurant, Table, Wait
 
 def seed_data():
     #restaurants
-    Shake_key = Restaurant(name = "Shake Shack", phone = "3234883010", street_address = "8520 Santa Monica Blvd", city = "West Hollywood", state = "CA", zip_code = "90069", user_email = "example@gmail.com").put()
-    in_key = Restaurant(name = "In n Out", phone = "8007861000", street_address = "13425 Washington Blvd", city = "Marina Del Rey", state = "CA", zip_code = "90292", user_email = "example@gmail.com").put()
-    cheese_key = Restaurant(name = "Cheescake Factory", phone = "3102601296", street_address = "395 Santa Monica Place", city = "Santa Monica", state = "CA", zip_code = "90401", user_email = "example@gmail.com").put()
-    blaze_key = Restaurant(name = "Blaze Pizza", phone = "3103400638", street_address = "4114 Sepulveda Blvd", city = "Culver City", state = "CA", zip_code = "90230", user_email = "example@gmail.com").put()
+    Shake_key = Restaurant(name = "Shake Shack", phone = "3234883010", street_address = "8520 Santa Monica Blvd", city = "West Hollywood", state = "CA", zip_code = "90069", user = "example@gmail.com").put()
+    in_key = Restaurant(name = "In n Out", phone = "8007861000", street_address = "13425 Washington Blvd", city = "Marina Del Rey", state = "CA", zip_code = "90292", user = "example@gmail.com").put()
+    cheese_key = Restaurant(name = "Cheescake Factory", phone = "3102601296", street_address = "395 Santa Monica Place", city = "Santa Monica", state = "CA", zip_code = "90401", user = "example@gmail.com").put()
+    blaze_key = Restaurant(name = "Blaze Pizza", phone = "3103400638", street_address = "4114 Sepulveda Blvd", city = "Culver City", state = "CA", zip_code = "90230", user = "example@gmail.com").put()
 
     #tables
     shake1_table_key = Table(max = "5", min = "2", full = False).put()
