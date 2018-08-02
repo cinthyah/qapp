8f22d88b50dc57fb1a8401dc53031455c841c7a6
Cinthya Hernandez <demouser@MacBook-Pro.local>
2018-08-01 17:19:25 -0700

    allows user to delete customer

--- seed_q.py
+++ seed_q.py
@@ -3,10 +3,10 @@ import datetime
 
 def seed_data():
     #restaurants
-    Shake_key = Restaurant(name = "Shake Shack", phone = "3234883010", street_address = "8520 Santa Monica Blvd", city = "West Hollywood", state = "CA", zip_code = "90069", user = "example@gmail.com").put()
-    in_key = Restaurant(name = "In n Out", phone = "8007861000", street_address = "13425 Washington Blvd", city = "Marina Del Rey", state = "CA", zip_code = "90292", user = "example@gmail.com").put()
-    cheese_key = Restaurant(name = "Cheescake Factory", phone = "3102601296", street_address = "395 Santa Monica Place", city = "Santa Monica", state = "CA", zip_code = "90401", user = "example@gmail.com").put()
-    blaze_key = Restaurant(name = "Blaze Pizza", phone = "3103400638", street_address = "4114 Sepulveda Blvd", city = "Culver City", state = "CA", zip_code = "90230", user = "example@gmail.com").put()
+    shake_key = Restaurant(name = "Shake Shack", phone = "3234883010", street_address = "8520 Santa Monica Blvd", city = "West Hollywood", state = "CA", zip_code = "90069", user = "shake@gmail.com").put()
+    in_key = Restaurant(name = "In n Out", phone = "8007861000", street_address = "13425 Washington Blvd", city = "Marina Del Rey", state = "CA", zip_code = "90292", user = "innout@gmail.com").put()
+    cheese_key = Restaurant(name = "Cheescake Factory", phone = "3102601296", street_address = "395 Santa Monica Place", city = "Santa Monica", state = "CA", zip_code = "90401", user = "cheesecake@gmail.com").put()
+    blaze_key = Restaurant(name = "Blaze Pizza", phone = "3103400638", street_address = "4114 Sepulveda Blvd", city = "Culver City", state = "CA", zip_code = "90230", user = "blaze@gmail.com").put()
 
     #tables
     shake1_table_key = Table(max = "5", min = "2", full = False).put()
